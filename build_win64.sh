044022276476340582a9de7cd1a4c396bd2adbe4
Florin Popescu <florinsgpopescu@gmail.com>
2020-06-06 00:40:12 +0300

    use single variable for install dir

--- build_win64.sh
+++ build_win64.sh
@@ -1,4 +1,11 @@
+$INSTALL_DIR=/home/avrdude
+
 ./bootstrap
-./configure --prefix=/home/avrdude --datadir=/home/avrdude --sysconfdir=/home/avrdude/bin
+
+./configure \
+--prefix=$INSTALL_DIR \
+--datadir=$INSTALL_DIR \
+--sysconfdir=$INSTALL_DIR/bin
+
 make all -j $NUMBER_OF_PROCESSORS
 make install
