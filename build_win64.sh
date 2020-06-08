002de6f85e806dc5563a5a10faa3a1ac99945d7b
Florin Popescu <florinsgpopescu@gmail.com>
2020-06-08 19:37:32 +0300

    build using mingw compiler

--- build_win64.sh
+++ build_win64.sh
@@ -5,7 +5,8 @@ INSTALL_DIR=/home/avrdude
 ./configure \
 --prefix=$INSTALL_DIR \
 --datadir=$INSTALL_DIR \
---sysconfdir=$INSTALL_DIR/bin
+--sysconfdir=$INSTALL_DIR/bin \
+--host=x86_64-w64-mingw32
 
 make all -j $NUMBER_OF_PROCESSORS
 make install
