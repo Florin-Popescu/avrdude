172b01a142a502601d3d4268e98ae3be6ea3a1f4
Florin Popescu <florinsgpopescu@gmail.com>
2020-06-10 01:03:51 +0300

    switched to msys2 environment

--- build_msys2.sh
+++ build_msys2.sh
@@ -1,12 +1,12 @@
-INSTALL_DIR=/home/avrdude
+INSTALL_DIR=/c/avr
 
 ./bootstrap
 
+CC=x86_64-w64-mingw32-gcc \
 ./configure \
 --prefix=$INSTALL_DIR \
 --datadir=$INSTALL_DIR \
---sysconfdir=$INSTALL_DIR/bin \
---host=x86_64-w64-mingw32
+--sysconfdir=$INSTALL_DIR/bin
 
 make all -j $NUMBER_OF_PROCESSORS
 make install
