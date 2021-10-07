c903701ad25fa65438438820db6d7653e5c08959
Florin Popescu <florinsgpopescu@gmail.com>
2021-10-07 17:33:05 +0300

    add dependencies to be built

--- build.sh
+++ build.sh
@@ -2,6 +2,23 @@
 
 case $(uname -o | cut -d '/' -f2) in
 	"Msys")
+		#These libraries are not available pre-built under msys2
+		git clone git@github.com:libusb/libusb.git
+		cd ./libusb
+		./bootstrap.sh
+		./configure
+		make -j$NUMBER_OF_PROCESSORS
+		make install
+		cd ..
+
+		git clone git@github.com:libusb/hidapi.git
+		cd ./hidapi
+		./bootstrap
+		./configure
+		make all -j$NUMBER_OF_PROCESSORS
+		make install
+		cd ..
+
 		SUDO=
 		;;
 	"Linux")
