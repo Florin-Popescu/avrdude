b6eaa492668c3ec248cc8d0ab0a0fb225b1e9c32
Florin Popescu <florinsgpopescu@gmail.com>
2021-10-07 18:56:59 +0300

    add libusb-compat-0.1

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 
 case $(uname -o | cut -d '/' -f2) in
 	"Msys")
+		wget https://github.com/Florin-Popescu/libusb-compat-0.1/releases/download/v0.1.7/libusb-compat-0.1_msys_x86_64.zip -O libusb-compat-0.1_msys_x86_64.zip
+		unzip libusb-compat-0.1_msys_x86_64.zip -o
+		cp libusb-compat-0.1_msys_x86_64/* / -r
+
 		#These libraries are not available pre-built under msys2
 		git clone git@github.com:libusb/libusb.git
 		cd ./libusb
