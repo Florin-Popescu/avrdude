60bb26295fd038251aa94b74160731e84d388c57
Florin Popescu <florinsgpopescu@gmail.com>
2022-10-08 01:18:04 +0200

    simplify script

--- package.sh
+++ package.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-TOOL=avrdude
-ARCH=$(uname -m)
-OS=$(uname -o | cut -d '/' -f2 | tr '[:upper:]' '[:lower:]')
-UPSTREAM_VERSION=6.3
-
 if [ -x "$(command -v dpkg-deb)" ]; then
 	DISTRO=deb
 elif [ -x "$(command -v makepkg)" ]; then
 	DISTRO=arch
 fi
 
-PACKAGE="$TOOL"_"$OS"_"$DISTRO"_"$ARCH"
+PACKAGE="$TOOL"_"$ARCH"
 DESTDIR=./"$PACKAGE"
 
 
@@ -20,8 +15,5 @@ if [ $DISTRO == deb ]; then
 	make install
 	dpkg-deb --build --root-owner-group $PACKAGE
 elif [ $DISTRO == arch ]; then
-	tar --create --gzip --file="$PACKAGE"/"$TOOL"-"$UPSTREAM_VERSION".tar.gz .
-	cd $PACKAGE
 	makepkg
-	cp *.zst ../$PACKAGE.pkg.tar.zst
 fi
